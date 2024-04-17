b30b01b932cf2627d499f8d927ec3dbcf16e8957
Luis Otávio <luisjralves_2014@hotmail.com.br>
2024-04-17 05:17:44 -0300

    mudando rota pra post

--- src/routes/users/user.routes.ts
+++ src/routes/users/user.routes.ts
@@ -6,4 +6,4 @@ import { registerUser, verifyLogin } from '../../controllers/user.controller';
 export const userRouter = express();
 
 userRouter.route('/register').post((req: Request, res: Response) => registerUser(req, res));
-userRouter.route('/login').get((req: Request, res: Response) => verifyLogin(req, res));
+userRouter.route('/login').post((req: Request, res: Response) => verifyLogin(req, res));
